6bba00171ca6274f6e2abc6f684eee23d39041b8
Shin Juheon <juheon8794@gmail.com>
2025-05-15 17:32:14 +0900

    2025.05.15

--- _NoteBox/src/Main.java
+++ _NoteBox/src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
+import java.util.ArrayList;
 
-    }
+public class Main {
+        static ArrayList<String> memo = new ArrayList<>();
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int num;
+
+            do {
+                System.out.print(
+                        """
+                        -- NoteBox --
+                        1. 할 일 추가
+                        2. 할 일 삭제
+                        3. 할 일 보기
+                        4. 종료
+                        메뉴 선택: """ );
+                num = sc.nextInt();
+                sc.nextLine();
+            } while (num != 2);
+        }
 }
