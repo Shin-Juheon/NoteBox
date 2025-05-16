a2925b84540e3d57388211431193acb75b93238c
Shin Juheon <juheon8794@gmail.com>
2025-05-16 22:51:01 +0900

    addTask 추가 ( 더  깔끔한 방법으로 코드 정렬 탐색중)

--- _NoteBox/src/Main.java
+++ _NoteBox/src/Main.java
@@ -2,34 +2,46 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
-        static ArrayList<String> memo = new ArrayList<>();
+    public static ArrayList<String> NoteBox = new ArrayList<>();
 
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int num;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
 
-            do {
-                System.out.print(
-                        """
-                        -- NoteBox --
-                        1. 할 일 추가
-                        2. 할 일 삭제
-                        3. 할 일 보기
-                        4. 종료
-                        메뉴 선택: """ );
-                num = sc.nextInt();
-                sc.nextLine();
+        do {
+            System.out.print(
+                    """
+                            -- NoteBox --
+                            1. 할 일 추가
+                            2. 할 일 삭제
+                            3. 할 일 보기
+                            4. 할 일 수정                       
+                            5. 종료
+                            메뉴 선택: """);
+            num = sc.nextInt();
+            sc.nextLine();
 
-                switch (num) {
-                    case 1:
-                        System.out.println("추가할 할 일 입력: ");
-                    case 2:
-                        System.out.println("프로그램을 종료합니다.");
-                        break;
-                    default:
-                        System.out.println("옳은 번호로 다시 시도해주세요.");
+            switch (num) {
+                case 1:
+                    System.out.println("추가할 할 일 입력: ");
+                    String memo = sc.nextLine();
+                    addTask(memo);
+                    break;
+                case 2:
+                    System.out.println("프로그램을 종료합니다.");
+                    break;
+                default:
+                    System.out.println("옳은 번호로 다시 시도해주세요.");
 
-                }
-            } while (num != 2);
-        }
+            }
+        } while (num != 2);
+
+        sc.close();
+    }
+    public static void addTask(String memo) {
+        NoteBox.add(memo);
+        System.out.println("할 일이 추가되었습니다: " + memo);
+    }
 }
+
+
